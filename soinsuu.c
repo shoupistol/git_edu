3a24c1c558bfdf2bf46fc059378f535ae779c2ca
shoupistol <watanabe.shouta0205@gmail.com>
2023-07-08 07:25:20 +0900

    soinsuu

---
+++ soinsuu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int num;
+    printf("‘fˆö”•ª‰ğ‚µ‚½‚¢”š ");
+    scanf("%d",&num);
+    for(int i = 2; i <= num; i++) {
+        if(num % i == 0) {
+            printf("%d\n",i);
+            num /= i;
+            i--; //‚à‚¤ˆê‰ñ”»’è‚³‚¹‚é
+        }
+    }
+
+}

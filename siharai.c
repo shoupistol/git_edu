f41444a8bdc9101a2ffb46a074f4e4c0fc24f211
shoupistol <watanabe.shouta0205@gmail.com>
2023-07-10 16:58:08 +0900

    okane

--- siharai.c
+++ siharai.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-    int money;
+    int money,hundred,ten,one;
+
     printf("Ç¢Ç≠ÇÁÅH ");
     scanf("%d",&money);
+
+    hundred = money / 100;
+    money %= 100;
+    ten = money / 10;
+    money %= 10;
+    one = money;
+
+    printf("100â~ : %dñá\n",hundred);
+    printf("10â~ : %dñá\n",ten);
+    printf("1â~ : %dñá\n",one);
+
     return 0;
 }
